1f23fdfae86c726674c6ec483788c52d17b56d9a
Nick Puetz <Napez702@hotmail.com>
2019-05-24 09:50:58 -0400

    added player class

---
+++ HeadSoccer/Classes/Player.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadSoccer.Classes
+{
+    class Player
+    {
+        public static int x, y, speed;
+        public static string direction;
+        int jumpHeight = 40, counter = 1;
+        bool flag = true;
+
+        public Player(int _x, int _y, int _speed, string _direction)
+        {
+            x = _x;
+            y = _y;
+            speed = _speed;
+            direction = _direction;
+        }
+
+        public void PlayerMove(string _direction)
+        {
+            if (_direction == "left")
+            {
+                x = x - speed;
+            }
+
+            if (_direction == "right")
+            {
+                y = y - speed;
+            }
+        }
+
+        public void PlayerJump()
+        {
+            if (flag == true)
+            {
+                flag = false;
+
+                if (counter == 1)
+                {
+                    y = y + 15;
+                }
+
+                if (counter == 2)
+                {
+                    y = y + 25;
+                }
+
+                if (y >= 710)
+                {
+                    flag = true;
+                }
+            }
+
+        }
+
+    }
+}
--- HeadSoccer/Screens/GameScreen.cs
+++ HeadSoccer/Screens/GameScreen.cs
@@ -7,18 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace HeadSoccer.Screens
 {
     public partial class GameScreen : UserControl
     {
         bool escDown = false;
+        List<Rectangle> hitBox = new List<Rectangle>();
 
         public GameScreen()
         {
             InitializeComponent();
             GameTimer.Enabled = true;
 
+            Rectangle player1 = new Rectangle(200, this.Height - 160, 100, 160);
+            Rectangle player2 = new Rectangle(900, this.Height - 160, 100, 160);
+
+            hitBox.Add(player1);
+            hitBox.Add(player2);
         }
 
         private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
@@ -43,7 +50,7 @@ namespace HeadSoccer.Screens
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+        
         }
 
         private void GameScreen_KeyUp(object sender, KeyEventArgs e)
