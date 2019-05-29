6a9c09e72a3249e33441f4a4b58245225b6b8f72
Tyler Tucker <tuckert19@gmail.com>
2019-05-29 09:00:19 -0400

    yes

--- HeadSoccer/Ball.cs
+++ HeadSoccer/Ball.cs
@@ -24,17 +24,17 @@ namespace HeadSoccer
             y = a*(x * x) + x + b;
         }
 
-        public bool BlockCollision(Player p)
-        {
-            Rectangle blockRec = new Rectangle(b.x, b.y, b.width, b.height);
-            Rectangle ballRec = new Rectangle(x, y, size, size);
+        //public bool BlockCollision(Player p)
+        //{
+        //    Rectangle blockRec = new Rectangle(b.x, b.y, b.width, b.height);
+        //    Rectangle ballRec = new Rectangle(x, y, size, size);
 
-            if (ballRec.IntersectsWith(blockRec))
-            {
-                string side = CollisionSide(blockRec);
-            }
+        //    if (ballRec.IntersectsWith(blockRec))
+        //    {
+        //        string side = CollisionSide(blockRec);
+        //    }
 
-            return blockRec.IntersectsWith(ballRec);
-        }
+        //    return blockRec.IntersectsWith(ballRec);
+        //}
     }
 }
